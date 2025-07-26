88b374d02a46fdd7f9c55cbd9622cc2f5e47cb3f
agha-seyed <agha.seyed.ehsan@gmail.com>
2025-07-26 17:15:19 +0200

    Update config.py

--- studentbot/config.py
+++ studentbot/config.py
@@ -5,17 +5,31 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
+# ✳️ Logger
+logger = logging.getLogger("StudentBot")
+logger.setLevel(logging.INFO)
+
+# Console log
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+logger.addHandler(console_handler)
+
+# File log (rotating)
+file_handler = RotatingFileHandler("studentbot.log", maxBytes=5*1024*1024, backupCount=3)
+file_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+logger.addHandler(file_handler)
+
 # ✳️ بررسی متغیرهای محیطی
 def validate_env_vars():
     required_vars = [
-        "TELEGRAM_BOT_TOKEN",  # استفاده از نام متغیر در Render
+        "TELEGRAM_BOT_TOKEN",
         "REDIS_URL",
         "GOOGLE_CREDS",
         "ADMIN_CHAT_ID",
         "BASE_URL",
         "WEBHOOK_SECRET",
         "QUESTIONS_SHEET_NAME",
-        "OPENWEATHERMAP_API_KEY",  # استفاده از نام متغیر در Render
+        "OPENWEATHERMAP_API_KEY",
         "PORT"
     ]
     missing_vars = []
@@ -40,20 +54,6 @@ WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
 QUESTIONS_SHEET_NAME = os.getenv("QUESTIONS_SHEET_NAME")
 WEATHER_API_KEY = os.getenv("OPENWEATHERMAP_API_KEY")
 PORT = int(os.getenv("PORT", 8080))
-
-# ✳️ Logger
-logger = logging.getLogger("StudentBot")
-logger.setLevel(logging.INFO)
-
-# Console log
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
-logger.addHandler(console_handler)
-
-# File log (rotating)
-file_handler = RotatingFileHandler("studentbot.log", maxBytes=5*1024*1024, backupCount=3)
-file_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
-logger.addHandler(file_handler)
 
 # ✳️ بررسی وجود فایل Google Credentials
 def verify_google_creds():
